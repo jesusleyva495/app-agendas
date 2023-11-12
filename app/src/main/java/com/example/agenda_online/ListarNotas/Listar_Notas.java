c5e2387c0d90498e63bd74900f277b40d1b2890b
DanielRiosM <56373511+DanielRiosM@users.noreply.github.com>
2023-11-11 20:56:57 -0700

    Ya se puede agregar notas y se pueden ver esas notas

--- app/src/main/java/com/example/agenda_online/ListarNotas/Listar_Notas.java
+++ app/src/main/java/com/example/agenda_online/ListarNotas/Listar_Notas.java
@@ -1,16 +1,110 @@
 package com.example.agenda_online.ListarNotas;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.example.agenda_online.Objetos.Nota;
 import com.example.agenda_online.R;
+import com.example.agenda_online.ViewHolder.ViewHolder_Nota;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
 
 public class Listar_Notas extends AppCompatActivity {
 
+    RecyclerView recyclerviewNotas;
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference BASE_DE_DATOS;
+    LinearLayoutManager linearLayoutManager;
+
+    FirebaseRecyclerAdapter<Nota, ViewHolder_Nota> firebaseRecyclerAdapter;
+    FirebaseRecyclerOptions<Nota> options;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_listar_notas);
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setTitle("Mis notas");
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setDisplayShowHomeEnabled(true);
+        }
+
+        recyclerviewNotas = findViewById(R.id.recyclerviewNotas);
+        recyclerviewNotas.setHasFixedSize(true);
+
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        BASE_DE_DATOS = firebaseDatabase.getReference("Notas_Publicadas");
+        ListarNotasUsuarios();
+    }
+
+    private void ListarNotasUsuarios(){
+        options = new FirebaseRecyclerOptions.Builder<Nota>().setQuery(BASE_DE_DATOS, Nota.class).build();
+        firebaseRecyclerAdapter = new FirebaseRecyclerAdapter<Nota, ViewHolder_Nota>(options) {
+            @Override
+            protected void onBindViewHolder(@NonNull ViewHolder_Nota viewHolder_nota, int position, @NonNull Nota nota) {
+                viewHolder_nota.SetearDatos(
+                        getApplicationContext(),
+                        nota.getId_nota(),
+                        nota.getUid_usuario(),
+                        nota.getCorreo_usuario(),
+                        nota.getFecha_hora_actual(),
+                        nota.getTitulo(),
+                        nota.getDescripcion(),
+                        nota.getFecha_nota(),
+                        nota.getEstado()
+                );
+            }
+
+            @Override
+            public ViewHolder_Nota onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_nota,parent,false);
+                ViewHolder_Nota viewHolder_nota = new ViewHolder_Nota(view);
+                viewHolder_nota.setOnClickListener(new ViewHolder_Nota.ClickListener() {
+                    @Override
+                    public void onItemClick(View view, int position) {
+                        Toast.makeText(Listar_Notas.this, "on item click", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onItemLongClick(View view, int position) {
+                        Toast.makeText(Listar_Notas.this, "on item long click", Toast.LENGTH_SHORT).show();
+                    }
+                });
+                return viewHolder_nota;
+            }
+        };
+
+        linearLayoutManager = new LinearLayoutManager(Listar_Notas.this, LinearLayoutManager.VERTICAL, false);
+        linearLayoutManager.setReverseLayout(true);
+        linearLayoutManager.setStackFromEnd(true);
+
+        recyclerviewNotas.setLayoutManager(linearLayoutManager);
+        recyclerviewNotas.setAdapter(firebaseRecyclerAdapter);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (firebaseRecyclerAdapter!=null){
+            firebaseRecyclerAdapter.startListening();
+        }
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return super.onSupportNavigateUp();
     }
 }
